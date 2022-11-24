cccd27a50a493d118e59401d1228302a6fa8c1e8
Reem <apple@Apples-MacBook-Pro.local>
2022-11-25 04:22:05 +0600

    header done

--- src/Pages/HomePage/ComponentsOfHomePage/Slider.js
+++ src/Pages/HomePage/ComponentsOfHomePage/Slider.js
@@ -3,8 +3,45 @@ import React from 'react';
 const Slider = () => {
     return (
         <div>
-            slider
-        </div>
+
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+
+                    <div className='w-full' style={{
+                        backgroundImage: `url("https://i.ibb.co/8PSpYwL/Mens.jpg")`, height: '100vh',
+                        fontSize: '50px',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                    }}>
+                        <div className='text-white text-center mt-64 font-extrabold'>
+                            <h5>Welcome to thrift shop!</h5>
+                            <p>Here you can bye or sell gently used clothes at discounted price!</p>
+                        </div>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" ></a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <div className='w-full' style={{
+                        backgroundImage: `url("https://i.ibb.co/pbJ0HW1/Womens.jpg")`, height: '100vh',
+                        fontSize: '50px',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                    }}>
+                        <div className='text-white text-center mt-64 font-extrabold'>
+                            <h5>Have any clothes to sell?</h5>
+                            <p>Create a seller account and start selling!</p>
+                        </div>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide1" ></a>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Slider from './ComponentsOfHomePage/Slider';
 
 const Home = () => {
     return (
         <div>
-            home
+            <Slider></Slider>
         </div>
     );
 };
