4779ab27bbf1ce4e08c88c4d46440c8f42fe986e
Reem <apple@Apples-MacBook-Pro.local>
2022-11-29 04:07:26 +0600

    all buyers and all sellers done

--- src/Pages/DashboardPage/ComponentsOfDashboardPage/AllBuyers.js
+++ src/Pages/DashboardPage/ComponentsOfDashboardPage/AllBuyers.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import Loader from '../../../SharedComponents/Loader/Loader';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const AllBuyers = () => {
+    const [buyers, setBuyers] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:5000/users/buyer')
+            .then(data => setBuyers(data.data))
+    }, [])
     return (
         <div>
-            AllBuyers
-            <Loader></Loader>
+            <h3 className="text-3xl mb-5">All Buyers</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            buyers?.map((buyer, i) =>
+                                <tr className="hover" key={i}>
+                                    <th>{i + 1}</th>
+                                    <td>{buyer.name}</td>
+                                    <td>{buyer.email}</td>
+                                    <td><button className='btn btn-xs btn-danger'>Delete</button></td>
+                                </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/DashboardPage/ComponentsOfDashboardPage/AllSellers.js
+++ src/Pages/DashboardPage/ComponentsOfDashboardPage/AllSellers.js
@@ -1,9 +1,44 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const AllSellers = () => {
+    const [sellers, setSellers] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:5000/users/seller')
+            .then(data => setSellers(data.data))
+    }, [])
     return (
         <div>
-            AllSellers
+            <h3 className="text-3xl mb-5">All Sellers</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Verified</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            sellers?.map((seller, i) =>
+                                <tr className="hover" key={i}>
+                                    <th>{i + 1}</th>
+                                    <td>{seller.name}</td>
+                                    <td>{seller.email}</td>
+                                    {
+                                        seller?.verified ?
+                                            <td>Yes</td>
+                                            :
+                                            <td>No</td>
+                                    }
+                                    <td><button className='btn btn-xs btn-danger'>Delete</button></td>
+                                </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
